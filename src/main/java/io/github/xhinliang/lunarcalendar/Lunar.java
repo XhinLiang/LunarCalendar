c96b0ee903f1b1a1a31cbeb987fc0ff01f73d2ae
xhinliang <xhinliang@gmail.com>
2016-02-08 12:46:50 +0800

    recompile in Java 6

---
+++ src/main/java/io/github/xhinliang/lunarcalendar/Lunar.java
@@ -0,0 +1,23 @@
+package io.github.xhinliang.lunarcalendar;
+
+/**
+ * Created by io.xhinliang on 16-2-6.
+ *
+ * @author XhinLiang 2016-02-06
+ */
+public class Lunar extends Gregorian {
+    public boolean isLeap;
+
+    Lunar(int y, int m, int d) {
+        super(y, m, d);
+    }
+
+    @Override
+    public String toString() {
+        return "Lunar{" +
+                "year=" + year +
+                ", month=" + month +
+                ", day=" + day +
+                '}';
+    }
+}
