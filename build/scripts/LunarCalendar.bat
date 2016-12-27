2e0498955c7ea723b2313c582d18a46abb52c79d
XhinLiang <xhinliang@gmail.com>
2016-12-27 20:03:00 +0800

    fix 节气

--- build/scripts/LunarCalendar.bat
+++ build/scripts/LunarCalendar.bat
@@ -8,13 +8,13 @@
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and LUNAR_CALENDAR_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS=
-
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%..
+
+@rem Add default JVM options here. You can also use JAVA_OPTS and LUNAR_CALENDAR_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
@@ -46,7 +46,7 @@ echo location of your Java installation.
 goto fail
 
 :init
-@rem Get command-line arguments, handling Windowz variants
+@rem Get command-line arguments, handling Windows variants
 
 if not "%OS%" == "Windows_NT" goto win9xME_args
 if "%@eval[2+2]" == "4" goto 4NT_args
@@ -69,7 +69,7 @@ set CMD_LINE_ARGS=%$
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\LunarCalendar-1.1.0.jar
+set CLASSPATH=%APP_HOME%\lib\LunarCalendar-1.2.2.jar
 
 @rem Execute LunarCalendar
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %LUNAR_CALENDAR_OPTS%  -classpath "%CLASSPATH%" io.github.xhinliang.lunarcalendar.Main %CMD_LINE_ARGS%
